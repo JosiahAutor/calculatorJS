9cb9e295c952f99070e3725c3607a3e8b2892ffc
Josiah Autor <autorjosiah@gmail.com>
2020-03-16 12:59:26 +0800

    add function for the buttons

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function dis(val){
+    document.getElementById('input').value+=val;
+    document.getElementById('input').focus();
+}
+function eq(val){
+    let i = document.getElementById('input').value;
+    let r = eval(i);
+    document.getElementById('input').value = `${r}`;
+}
